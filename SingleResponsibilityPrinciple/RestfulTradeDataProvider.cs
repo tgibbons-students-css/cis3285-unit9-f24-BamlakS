5ae4ae3d1c84e6d6d2f3e4fd406c3731a44e76fe
Bamlak Amedie <bamedie@css.edu>
2024-10-31 11:34:24 -0500

    Done with the learning activity.

--- SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
@@ -3,19 +3,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace SingleResponsibilityPrinciple
 {
     public class RestfulTradeDataProvider : ITradeDataProvider
     {
+        private readonly string _url;
+        private readonly ILogger _logger;
         public RestfulTradeDataProvider(string url, ILogger logger)
         {
+            _url = url;
+            _logger = logger;
+
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            return GetTradeDataAsync().Result;
+        }
+
+        private async Task<IEnumerable<string>> GetTradeDataAsync()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(_url);
+                    response.EnsureSuccessStatusCode();
+
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    var trades = JsonSerializer.Deserialize<List<string>>(jsonResponse);
+
+                    return trades ?? new List<string>();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInfo("Error fetching trade data: " + ex.Message);
+                return new List<string>();
+            }
         }
     }
 }
